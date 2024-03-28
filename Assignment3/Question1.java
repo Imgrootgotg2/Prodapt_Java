942202808ae37b36c94e9e2a6ed952feb8f65fa7
Imgrootgotg2 <shivaramancheenu2002@gmail.com>
2024-03-28 17:24:38 +0530

    my 2nd commit

--- Assignment3/Question1.java
+++ Assignment3/Question1.java
@@ -4,28 +4,38 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Question1 {
-	public static void main(String[] argv) throws Exception
-    {
-       
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Enter the name of the directory");
-       String path = sc.nextLine();
-       System.out.println("File name");
-       String filename = sc.nextLine();
-       File file = new File(path);
-       int k=0;
-       if(file.isDirectory());
-       File []files=file.listFiles();
-       for(File file1:files) {
-    	   if(file1.isFile()&& file1.getName().equals(filename)) {
-    		   System.out.println("File found");
-    		   k=1;
-    		   break;
-    	   }
-    	   sc.close();	       
-       }
-       if(k==0) {
- 		  System.out.println("File not found");
- 	   }
+	public static void main(String[] args) {
+        //Getting input from user
+    	Scanner scan = new Scanner(System.in);
+ 
+        System.out.print("Enter the file extension: ");
+        String fileExtension = scan.nextLine();
+ 
+        System.out.print("Enter the directory path to search: ");
+        String searchDirectory = scan.nextLine();
+        int k=0;
+        //Operations or Process starts
+        File dir = new File(searchDirectory);
+        String[] filesInDirectory = null;
+        if(dir.isDirectory()) {
+        	filesInDirectory = dir.list();
+        }
+//        for(String s : filesInDirectory) {
+//        System.out.println(s);
+//        }
+//       
+        if (filesInDirectory != null) {
+            System.out.println("Matching files:");
+            for (String sFile : filesInDirectory) {
+            	String[] fileDetails = sFile.split("[.]");
+            	if((fileDetails[1]).equals(fileExtension)) {
+            		k=1;
+            		System.out.println("File found with name : "+sFile);
+            	}
+            }
+        }
+        if(k==0){
+            System.out.println("No files found in the specified directory.");
+        }
     }
 }
